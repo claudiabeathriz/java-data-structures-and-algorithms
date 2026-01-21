751355bfd5e8c396738543457043949ca85dd2bc
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-01-21 13:32:47 -0300

    Include explanataion to hashCode() and equals()

--- hash-code-and-equals/application/HashEqualsProgram.java
+++ hash-code-and-equals/application/HashEqualsProgram.java
@@ -9,6 +9,9 @@ public class HashEqualsProgram {
         Client c1 = new Client("Maria", "maria@email.com");
         Client c2 = new Client("Maria", "maria@email.com");
 
+        String s1 = "Test";
+        String s2 = "Test";
+
         System.out.println(c1.hashCode());
         System.out.println(c2.hashCode());
         //  typically derives an integer from the object's memory address
@@ -17,7 +20,12 @@ public class HashEqualsProgram {
         //  you must also override hashCode() to maintain the contract.
         System.out.println(c1.equals(c2));
         // compares the content/value of objects.
-        System.out.println(c1 = c2);
+        System.out.println(c1 == c2);
         // compares memory addresses
+        // returns "false" because "new" always creates a new object
+        System.out.println(s1 == s2);
+        // returns "true" because of String Pool
+        // String literals are stored in a special area of memory
+        // Java reuses the same String objects to save memory
     }
 }

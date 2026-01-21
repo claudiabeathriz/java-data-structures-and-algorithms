146c67885b6743f7cf8920af5560bc2c0857aaac
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2026-01-21 13:17:16 -0300

    Add hashCode() and equals()

---
+++ hash-code-and-equals/application/HashEqualsProgram.java
@@ -0,0 +1,23 @@
+package application;
+
+import entities.Client;
+
+public class HashEqualsProgram {
+
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Maria", "maria@email.com");
+        Client c2 = new Client("Maria", "maria@email.com");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        //  typically derives an integer from the object's memory address
+        //  p.s. when you override the equals() method in a custom class to define
+        //  logical equality (e.g., two Person objects are equal if they have the same name and ID),
+        //  you must also override hashCode() to maintain the contract.
+        System.out.println(c1.equals(c2));
+        // compares the content/value of objects.
+        System.out.println(c1 = c2);
+        // compares memory addresses
+    }
+}
